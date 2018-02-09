18a83e6ac2a2cf23c9b3860f2c822a3aa08ac71b
Kate McHugh-Westfall <kate.mchugh94@gmail.com>
2018-02-09 17:33:14 +0000

    automatically backed up by learn

--- bma_current_exhibit_cli_app/lib/bma_current_exhibit_cli_app/cli.rb
+++ bma_current_exhibit_cli_app/lib/bma_current_exhibit_cli_app/cli.rb
@@ -6,11 +6,31 @@
     puts" Welcome, art lover!"
     puts" Here are the current exhibitions at the Baltimore Museum of Art"
     list_exhibits
+    menu
   end
-
+  #lists exhibits from museum. Will change to scraped info one scraper class built
   def list_exhibits
     puts " 1) Vangogh - Through July 2017"
     puts " 2) Monet - Through Oct 2018"
     puts " 3) Warhol- Through Nov 2018"
   end
+  #gives user the option to get more info about each exhibit, go back to menu, or exit
+  def menu
+    puts "Enter the number of the exhibit to learn more about it. If you want to see the list again, enter 'list'. If you want to exit, type 'exit'." 
+    input = gets.strip
+      if input == "1"
+        puts " Cut off his ear"
+      elsif input == "2"
+          puts "swishy paint man"
+        elsif input == "3"
+          puts "15 minutes of fame"
+        elsif input == "list"
+          list_exhibits
+          menu
+        else
+          puts" Thanks for checking this out! See you next time!"
+          exit
+        end
+      end
+           
  end
