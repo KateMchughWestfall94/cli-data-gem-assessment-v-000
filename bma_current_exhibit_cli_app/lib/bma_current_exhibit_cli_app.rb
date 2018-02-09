786089bf17b4590b36c042983f77ece5c8fb9bcb
Kate McHugh-Westfall <kate.mchugh94@gmail.com>
2018-02-09 17:13:35 +0000

    automatically backed up by learn

--- bma_current_exhibit_cli_app/lib/bma_current_exhibit_cli_app.rb
+++ bma_current_exhibit_cli_app/lib/bma_current_exhibit_cli_app.rb
@@ -1,2 +1,2 @@
-require "bma_current_exhibit_cli_app/version"
-require "bma_current_exhibit_cli_app/cli"
+require_relative "bma_current_exhibit_cli_app/version"
+require_relative "bma_current_exhibit_cli_app/cli"
