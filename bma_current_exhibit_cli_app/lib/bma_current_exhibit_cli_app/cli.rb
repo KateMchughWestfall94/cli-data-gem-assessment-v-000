caa9bee1a03a2a7cb4684a0cd17250a3b47fbdc4
Kate McHugh-Westfall <kate.mchugh94@gmail.com>
2018-02-09 17:23:27 +0000

    automatically backed up by learn

--- bma_current_exhibit_cli_app/lib/bma_current_exhibit_cli_app/cli.rb
+++ bma_current_exhibit_cli_app/lib/bma_current_exhibit_cli_app/cli.rb
@@ -3,8 +3,8 @@
  class BmaCurrentExhibitCliApp::CLI
    #welcomes the user, lets them know what they are about to see, and lists the exhibits
   def call
-    puts"Welcome, art lover!"
-    puts"Here are the current exhibitions ate the Baltimore Museum of Art"
+    puts" Welcome, art lover!"
+    puts" Here are the current exhibitions at the Baltimore Museum of Art"
     list_exhibits
   end
 
