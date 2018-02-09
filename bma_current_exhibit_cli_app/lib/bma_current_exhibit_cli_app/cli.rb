11cf71bdd944e5c0e23cc3b022d2f23ec60912f9
Kate McHugh-Westfall <kate.mchugh94@gmail.com>
2018-02-09 17:19:59 +0000

    automatically backed up by learn

--- bma_current_exhibit_cli_app/lib/bma_current_exhibit_cli_app/cli.rb
+++ bma_current_exhibit_cli_app/lib/bma_current_exhibit_cli_app/cli.rb
@@ -1,5 +1,6 @@
  class BmaCurrentExhibitCliApp::CLI
   def call
+    puts"Welcome, 
     puts "1) Vangogh - Through July 2017"
     puts "2) Monet - Through Oct 2018"
     puts "3) Warhol- Through Nov 2018"
