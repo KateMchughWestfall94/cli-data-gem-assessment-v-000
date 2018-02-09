53829815f181348a477e1295c51e4af71874bd75
Kate McHugh-Westfall <kate.mchugh94@gmail.com>
2018-02-09 17:22:49 +0000

    automatically backed up by learn

--- bma_current_exhibit_cli_app/lib/bma_current_exhibit_cli_app/cli.rb
+++ bma_current_exhibit_cli_app/lib/bma_current_exhibit_cli_app/cli.rb
@@ -4,12 +4,13 @@
    #welcomes the user, lets them know what they are about to see, and lists the exhibits
   def call
     puts"Welcome, art lover!"
-    puts"Here are the current exhibitions ate the Baltimore Museum of Art" 
+    puts"Here are the current exhibitions ate the Baltimore Museum of Art"
     list_exhibits
   end
+
   def list_exhibits
     puts "1) Vangogh - Through July 2017"
     puts "2) Monet - Through Oct 2018"
     puts "3) Warhol- Through Nov 2018"
-
+  end
  end
